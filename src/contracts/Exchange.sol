bb916e6d415d2035ca5439b1077a6e1a2e42f47f
Emmanuel Douge <edouge@gmail.com>
2021-12-08 10:39:43 -0700

    Start of the Exchange pt. Fee Account

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+// TODO:
+// [ ] Set the Fee Account
+// [ ] Deposit Ether
+// [ ] Withdraw Ether
+// [ ] Deposit Tokens
+// [ ] Withdraw Tokens
+// [ ] Check Balances
+// [ ] Make Order
+// [ ] Cancel Order
+// [ ] Fill Order
+// [ ] Charge Fees
+
+
+contract Exchange {
+    // Variables 
+    address public feeAccount; // The account that recvs exchange fees
+
+    constructor (address _feeAccount) public {
+        // State vs Local variable problem soled here...
+        feeAccount = _feeAccount;
+    }
+}
+
