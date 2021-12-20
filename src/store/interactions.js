47a345ce9461f788d9e9aa4f4d2bfb4d438b585c
Emmanuel Douge <edouge@gmail.com>
2021-12-20 10:19:39 -0700

    Redux

---
+++ src/store/interactions.js
@@ -0,0 +1,31 @@
+import Web3 from 'web3'
+import {
+  web3Loaded,
+  web3AccountLoaded
+} from './actions'
+import Token from '../abis/Token.json'
+import Exchange from '../abis/Exchange.json'
+
+
+export const loadWeb3 = async (dispatch) => {
+  if(typeof window.ethereum!=='undefined'){
+    const web3 = new Web3(window.ethereum)
+    dispatch(web3Loaded(web3))
+    return web3
+  } else {
+    window.alert('Please install MetaMask')
+    window.location.assign("https://metamask.io/")
+  }
+}
+
+export const loadAccount = async (web3, dispatch) => {
+  const accounts = await web3.eth.getAccounts()
+  const account = await accounts[0]
+  if(typeof account !== 'undefined'){
+    dispatch(web3AccountLoaded(account))
+    return account
+  } else {
+    window.alert('Please login with MetaMask')
+    return null
+  }
+}
